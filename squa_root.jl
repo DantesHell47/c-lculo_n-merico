b42445c8b79848f7e030b565f1922faf2abb4bee
DantesHell47 <Jefferson.dante47@gmail.com>
2023-12-04 22:51:36 -0300

    Passos iniciais de como implementar o método de Newton usando um loop for mesmo sabendo que um loop while é mais interessante para este trabalho

--- squa_root.jl
+++ squa_root.jl
@@ -1,13 +1,18 @@
 # Criar um algoritmo para calcular a raiz quadrada usando o método de Newton-Raphson
 # xₙ₊₁  = 1/2 * (xₙ + N / xₙ)
-# Sendo, xₙ₊₁ a próxima estimatica, xₙ é estimatica atual e N é o número que se deseja calcular a raiz quadrada
+# Sendo, xₙ₊₁ a próxima estimativa, xₙ é estimativa atual e N é o número que se deseja calcular a raiz quadrada
 
 
-N = 25
-x = N / 2
 
-for i in 1:10
-    x_p = 1 / 2 * (x + N / x)
-    x = x_p
-    println(x)
-end    
+function raiz_quad(N, num_max_iter)
+    x = N / 2
+    for i in 1:num_max_iter
+        x_p = 1 / 2 * (x + N / x)
+        x = x_p
+        println(x)
+    end    
+    
+end
+
+
+raiz_quad(144)
