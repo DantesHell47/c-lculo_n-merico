c20c01b4ffb4daa69c890189a3dbc0a057eebd5e
DantesHell47 <Jefferson.dante47@gmail.com>
2023-12-20 19:31:45 -0300

    Adicionado uma docstring para facilitar a leitura do código

--- squa_root.jl
+++ squa_root.jl
@@ -1,10 +1,25 @@
-# Criar um algoritmo para calcular a raiz quadrada usando o método de Newton-Raphson
-# xₙ₊₁  = 1/2 * (xₙ + N / xₙ)
-# Sendo, xₙ₊₁ a próxima estimativa, xₙ é estimativa atual e N é o número que se deseja calcular a raiz quadrada
 
 
+function raiz_quad1(N, num_max_iter=100)
+    """
+    Calcula a raiz quadrade aproximada de um numero
+    Este código utiliza o método de Newton-Raphson para calcular raiz quadrada aproximada.
+        Criar um algoritmo para calcular a raiz quadrada usando o método de Newton-Raphson
+        xₙ₊₁  = 1/2 * (xₙ + N / xₙ)
+        Sendo, 
+        xₙ₊₁: a próxima estimativa, 
+        xₙ: é estimativa atual 
+        N: é o número que se deseja calcular a raiz quadrada
 
-function raiz_quad(N, num_max_iter)
+    # Argumentos
+    - `N`: O numero que se deseja calcular a raiz quadrada.
+    - `num_max_iter = 100` Numero maximo de iteraçoes para aproximação. Definida como padrao igual a cem.
+
+    # Retorno 
+    - `x_p`: Retorna a raiz quadrada aproximada.
+    """
+
+
     if N < 0
         throw(DomainError("It's no possible"))
     end
