0de7487ca918ad8527a11f555b58bcb99b84ed5e
DantesHell47 <Jefferson.dante47@gmail.com>
2024-01-02 23:38:20 -0300

    O código foi refatorado. Foi também adicioanado outra condição de parada

--- squa_root.jl
+++ squa_root.jl
@@ -16,18 +16,17 @@ Este código utiliza o método de Newton-Raphson para calcular raiz quadrada apr
 - `x_p`: Retorna a raiz quadrada aproximada.
 """
 
-function raiz_quad(N, num_max_iter=100)
-    if N < 0
-        throw(DomainError("It's no possible"))
+function raiz_quad(n; num_max_iter=100)
+    if n < 0
+        throw(DomainError("Real square roots aren't defined for negative integers"))
     end
-    x = N / 2
+    x = n / 2
     iteracoes = 0
     while true
         iteracoes += 1
-        x_p = 1 / 2 * (x + N / x)
-        if iteracoes == num_max_iter || abs(x_p - x) < 1e-8
-            return x_p
-        end
+        x_p = 1 / 2 * (x + n / x)
+        iteracoes == num_max_iter || abs(x_p - x) < 1e-8 && return x_p
+
         x = x_p 
     end  
 end
